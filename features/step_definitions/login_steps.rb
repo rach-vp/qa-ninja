90b37a087e405ff989776a82f724ec48fb423be7
rach-vp <picanco.raquel@gmail.com>
2021-01-07 23:24:08 -0400

    Set Capybara app_host

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,10 +1,10 @@
-Dado('que acesso a página principal') do
-  visit 'http://rocklov-web:3000/'
+Dado("que acesso a página principal") do
+  visit "/"
 end
 
-Quando('submeto minhas credenciais com {string} e {string}') do |email, password|
-  find('#email').set email
-  find('#password').set password
+Quando("submeto minhas credenciais com {string} e {string}") do |email, password|
+  find("#email").set email
+  find("#password").set password
   # sleep 5
   click_button "Entrar"
   # sleep 5
--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,15 +1,15 @@
-Dado('que acesso a página de cadastro') do
-  visit 'http://rocklov-web:3000/signup'
+Dado("que acesso a página de cadastro") do
+  visit "/signup"
 end
 
-Quando('submeto o seguinte formulário de cadastro:') do |table|
+Quando("submeto o seguinte formulário de cadastro:") do |table|
   user = table.hashes.first
 
   DB_handling.new.remove_user(user[:email])
 
-  find('#fullName').set user[:name]
-  find('#email').set user[:email]
-  find('#password').set user[:password]
+  find("#fullName").set user[:name]
+  find("#email").set user[:email]
+  find("#password").set user[:password]
   # sleep 5
   click_button "Cadastrar"
 end
