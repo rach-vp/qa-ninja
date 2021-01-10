81408b202805ae9838aabc96814c3420cf030200
rach-vp <picanco.raquel@gmail.com>
2021-01-10 08:29:05 -0400

    Create new step definitions file

---
+++ features/step_definitions/create_annoucement_steps.rb
@@ -0,0 +1,12 @@
+Dado('que tenho o seguinte equipamento:') do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('submeto o cadastro desse item') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('devo ver esse item em meu Dashboard') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
